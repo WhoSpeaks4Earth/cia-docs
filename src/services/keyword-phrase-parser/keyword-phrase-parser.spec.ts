795badfba3c00caad7f6805c31369311d9878fa9
Shawn Bahramy <shawnbahramy@gmail.com>
2022-05-12 16:41:14 -0400

    eliminate redundant parser setup

--- src/services/keyword-phrase-parser/keyword-phrase-parser.spec.ts
+++ src/services/keyword-phrase-parser/keyword-phrase-parser.spec.ts
@@ -2,14 +2,19 @@ import { KeywordPhraseParserService } from './keyword-phrase-parser.service';
 
 
 describe('keyword/phrase parsing', () => {
+
+  let parser;
+
+  beforeEach(() => {
+    parser = new KeywordPhraseParserService();
+  })
+
   it('can parse single keyword', async () => {
-    const parser = new KeywordPhraseParserService();
     const text = 'term';
     expect(parser.getParsedTerms(text)).toEqual(['term']);
   });
 
   it('ignores redundant parsing characters', async () => {
-    const parser = new KeywordPhraseParserService();
     const variant1 = 'term1, term2';
     const variant2 = 'term1 ,,  term2';
     const expected = ['term1', 'term2'];
@@ -18,7 +23,6 @@ describe('keyword/phrase parsing', () => {
   });
 
   it('ignores leading and trailing parsing characters', async () => {
-    const parser = new KeywordPhraseParserService();
     const variant1 = '  term1, term2  ';
     const variant2 = ',,term1 term2,,';
     const variant3 = ', , term1 term2 , ,';
@@ -29,19 +33,16 @@ describe('keyword/phrase parsing', () => {
   });
 
   it('can parse multiple keywords separated by spaces', async () => {
-    const parser = new KeywordPhraseParserService();
     const text = 'term1 term2';
     expect(parser.getParsedTerms(text)).toEqual(['term1', 'term2']);
   });
 
   it('can parse multiple keywords separated by commas', async () => {
-    const parser = new KeywordPhraseParserService();
     const text = 'term1,term2';
     expect(parser.getParsedTerms(text)).toEqual(['term1', 'term2']);
   });
 
   it('can parse quoted phrases', async () => {
-    const parser = new KeywordPhraseParserService();
     const variant1 = `"term1" 'phrase1a phrase1b' term3`;
     const variant2 = `term1 "phrase1a phrase1b" "term3"`;
     const expected = ['term1', 'phrase1a phrase1b', 'term3'];
@@ -50,11 +51,8 @@ describe('keyword/phrase parsing', () => {
   });
 
   it('gives an empty array for bad input', async () => {
-    const parser = new KeywordPhraseParserService();
     const variant1 = `"term1" 'phrase1a phrase1b term3`;
     const variant2 = `term1 "phrase1a phrase1b' "term3"`;
-    // const variant3 = `term1 "phrase"oops "term3"`;
-    // const variant4 = `term1 oops"phrase" "term3"`;
     expect(parser.getParsedTerms(variant1)).toEqual([]);
     expect(parser.getParsedTerms(variant2)).toEqual([]);
   })
